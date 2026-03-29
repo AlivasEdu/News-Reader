dc532734fd915f5ee3a884de50ad71e866c5d40f
AlivasGR <alivanidis@gmail.com>
2026-03-29 12:20:01 +0300

    Vercel config update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import HeadlinesList from './components/HeadlinesList';
 import { fetchNews, Article, NewsResponse } from './lib/newsapi';
 
